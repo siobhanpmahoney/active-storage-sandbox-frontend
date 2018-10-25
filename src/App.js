1cdbcb4a092c44eb22b5bc29cb59f7c83518648e
siobhanpmahoney <siobhan.p.mahoney@gmail.com>
2018-10-25 18:03:34 -0400

    file metadata getting sent back ok. need to figure out how to retrieve files from db

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Dropzone from 'react-dropzone'
+import { DirectUpload } from "activestorage"
+import {fromEvent} from 'file-selector';
+
+
 
 
 class App extends Component {
-  
+
   constructor(props) {
     super(props)
 
@@ -26,12 +30,74 @@ class App extends Component {
   }
 
   onDrop = (f) => {
-    debugger
-    let updatedFiles = [...this.state.files, f]
-    this.setState({
-      files: updatedFiles
+    // const im = new Image()
+    // im.onload = () => {
+    //   const reader = new FileReader()
+    //   reader.onload = () => {
+    //
+    //   }
+    // }
+    //
+
+
+
+    //
+    // console.log(f)
+    // debugger
+    const promise = new Promise((resolve, reject) => {
+      const reader = new FileReader()
+      console.log("reader: ", reader)
+      reader.readAsDataURL(f[0])
+
+      reader.onload = () => {
+        if (!!reader.result) {
+          resolve(reader.result)
+        }
+        else {
+          reject(Error("Failed converting to base64"))
+        }
+      }
     })
+    // promise.then(result => {
+    //   console.log("f[0] before adding data_url: ", f[0])
+    //   f[0]["data_url"] = result
+    //   console.log("f[0] after adding data_url: ", f[0])
+    // }, err => {
+    //   console.log(err)
+    // })
+    // console.log("f[0] after adding data_url key: ", f[0] )
+    //
+    //
+    //
+    //
+    // let selectedUser = this.state.userSelectedForEdits
+    // selectedUser['avatar'] = f
+    // this.setState({
+    //   files: f,
+    //   userSelectedForEdits: selectedUser
+    // }, () => console.log(this.state.userSelectedForEdits))
+
+
+    let selectedUser = this.state.userSelectedForEdits
+    promise.then(result => {
+      console.log("result: ", result)
+      selectedUser['avatar'] = {data_url: result, title: f[0].name}
+      this.setState({
+        files: f,
+        userSelectedForEdits: selectedUser
+      }, () => console.log(this.state.userSelectedForEdits))
+    }, err => {
+      console.log(err)
+    })
+
+
+
+
+
+
   }
+
+
 
   onSelectEdit = (event) => {
     let u = this.state.users.find((user) => user.id == event.target.id)
@@ -54,16 +120,18 @@ class App extends Component {
   onSubmitEdits = (event) => {
     event.preventDefault()
     let userUpdates = this.state.userSelectedForEdits
+    // if (this.state.files.length > 0) {
+    //     userUpdates['avatar'] = this.state.files[0]
+    // }
+    console.log("in submit", userUpdates)
+
     fetch(`http://localhost:3000/api/v1/users/${userUpdates.id}`, {
       method: 'PATCH',
       headers: {
         'Content-type': 'application/json',
         'Accepts': 'application/json'
       },
-      body: JSON.stringify({
-        username: userUpdates.username,
-        location: userUpdates.location
-      })
+      body: JSON.stringify(userUpdates)
     })
     .then(response => response.json())
     .then(json => this.helperFindAndSplice(json))
@@ -117,15 +185,20 @@ class App extends Component {
               <div className="drop-zone-section">
                 <label> Avatar:
                   <div className="drop-area">
-                    <Dropzone onDrop={this.onDrop.bind(this)}>
+                    <Dropzone onDrop={this.onDrop.bind(this)} getDataTransferItems={evt => fromEvent(evt)} inputProps={{accept: "image/png, image/jpeg"}}>
+                      {this.state.files.length > 0 ? (
+                        <img src={this.state.files[0].preview} width="200px"/>
+                      ) : (
                       <p>Drop Files Here</p>
+                      )
+                    }
                     </Dropzone>
                   </div>
 
                   {this.state.files.length > 0 &&
                     <div className="drop-list">
                       {this.state.files.map((item) => {
-                        return <div className="file-item" key={item[0].name}>{item[0].name}</div>
+                        return <div className="file-item" key={item.name}>{item.name}</div>
                       })}
                     </div>
                   }
